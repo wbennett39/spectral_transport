61592a8d97a3ff74c746deaf3c7046949743660a
William Bennett <williamgbennett1@gmail.com>
2024-08-24 16:18:32 -0500

    crc

--- moving_mesh_transport/solver_classes/mesh.py
+++ moving_mesh_transport/solver_classes/mesh.py
@@ -870,7 +870,7 @@ class mesh_class(object):
                 min_space2 = 5 * min_space
                 # dx2 = min_space2 * third
                 # dx2 = self.x0/500
-                dx2 = 0.006
+                dx2 = 0.01
                 inside_wave_edges = self.x0 - (np.abs((np.logspace(0,1,third)-10)/-9) )*dx
 
                 # outside_wave_edges =  np.abs((np.flip((np.logspace(0,1,rest+1)-10)/-9) * (self.x0*self.l-dx))[:-1])

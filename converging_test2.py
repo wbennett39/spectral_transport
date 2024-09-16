07442d562de69ee9158120d350bff1b60cd82fc5
William Bennett <williamgbennett1@gmail.com>
2024-09-16 17:14:50 -0500

    crc

--- converging_test2.py
+++ converging_test2.py
@@ -62,8 +62,8 @@ run.boundary_source(0,0)
 run.load('marshak')
 run.parameters['boundary_source']['x0'] = np.array([0.05])
 
-# menis_times = np.array([-58.251607, -19.068532, -1])
-menis_times = np.array([-75, -80, -58.251607])
+menis_times = np.array([-58.251607, -19.068532, -1])
+# menis_times = np.array([-75, -80, -58.251607])
 dimensional_times =  85.4678 + menis_times 
 
 run.mesh_parameters['eval_array'] = dimensional_times * 29.98

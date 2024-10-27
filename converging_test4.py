3084ef0178adf393587d549f2c196626a4e4497e
William Bennett <williamgbennett1@gmail.com>
2024-10-26 22:26:56 -0600

    crc

--- converging_test4.py
+++ converging_test4.py
@@ -43,7 +43,7 @@ import numpy as np
 from moving_mesh_transport.solver_functions.run_functions import run
 
 
-N_spaces_list = [80, 90, 100, 150, 200, 250, 300, 350, 400]
+N_spaces_list = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400]
 # N_spaces_list = [10, 15, 20, 25, 50, 75, 100, 150, 200, 500, 1000]
 # N_spaces_list = [10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 run = run()

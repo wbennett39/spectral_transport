016d61695c84a76db41071055c62a05c944524b4
William Bennett <williamgbennett1@gmail.com>
2024-10-29 08:41:58 -0600

    CRC

--- moving_mesh_transport/solver_classes/opacity.py
+++ moving_mesh_transport/solver_classes/opacity.py
@@ -265,7 +265,7 @@ class sigma_integrator():
                 #     res = np.zeros(result.size) + resmax
             elif self.sigma_func['test4'] == 1:
                 floor = 5e-2
-                resmax = 1.9e4
+                resmax = 1.2e4
                 result = np.where(T_old<0.0, 0.0, T_old)
                 rho = np.mean(x )
                 if (x<0).any():

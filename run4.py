119b0103df3adbe103d21dbaf3fa8043f841cc5f
William Bennett <wbennett@lanl.gov>
2024-12-10 16:17:01 -0700

    crc

--- run4.py
+++ run4.py
@@ -1,4 +1,4 @@
 from converging_test4 import get_results
 
-get_results(ts =  [-94.706889, -27.126998, -1], N_spaces = [25, 50, 75, 100, 125, 150, 175, 200 ])
+get_results(ts =  [-94.706889, -27.126998, -1], N_spaces = [100, 150, 200 ], N_ang = 4)
 # get_results(ts =  [-145, -142, -140], N_spaces = [25, 50, 75, 100, 125, 150, 175, 200 ])

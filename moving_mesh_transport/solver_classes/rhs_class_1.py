904c20d7f8a26354eaf3bc9a04c88e78ebf7472f
William Bennett <wbennett@lanl.gov>
2024-12-10 15:31:02 -0700

    crc

--- moving_mesh_transport/solver_classes/rhs_class_1.py
+++ moving_mesh_transport/solver_classes/rhs_class_1.py
@@ -267,7 +267,7 @@ class rhs_class():
 
     def slope_scale(self, V, edges, stop = False):
         floor = -1e-8
-        posfloor = 1e-12
+        posfloor = 1e-10
         V_new = V.copy() 
         for k in range(self.N_space):
             h = math.sqrt(edges[k+1] - edges[k])
--- run4.py
+++ run4.py
@@ -1,3 +1,3 @@
 from converging_test4 import get_results
 
-get_results()
+get_results(N_spaces = [25, 50, 75, 100, 125, 150, 175, 200 ])

a43980069f1dce7651aff07427bae93dc614c1cb
William Bennett <wbennett@lanl.gov>
2025-07-10 15:51:48 -0600

    fixed keff spike

--- moving_mesh_transport/solver_functions/main_functions.py
+++ moving_mesh_transport/solver_functions/main_functions.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import math
 from .VDMD import VDMD2 as VDMD_func
 from pathlib import Path
+from scipy.interpolate import interp1d as interp
 from ..solver_classes.functions import find_nodes, normalize_phi
 from ..solver_classes.functions import Pn, normTn
 from .Chebyshev_matrix_reader import file_reader
@@ -283,15 +284,16 @@ def solve(tfinal, N_space, N_ang, M, N_groups, x0, t0, sigma_t, sigma_s, t_nodes
             norm_integrand = initialize.IC.copy()
             for space in range(N_space):
                 norm_integrand[:, space, :] = norm_integrand[:, space, :] * initialize.sigma_f[space] * initialize.nu[space] 
-            normalization = normalize_phi(norm_integrand, mesh.edges, ws, N_ang, M, N_space, N_groups)/ math.pi #/ 4 /math.pi /(mesh.edges[-1]**3-mesh.edges[0]**3) * 3
+            normalization = normalize_phi(norm_integrand, mesh.edges, ws, N_ang, M, N_space, N_groups) #/ 4 /math.pi /(mesh.edges[-1]**3-mesh.edges[0]**3) * 3
             # normalization = 1
+       
             # normalization = kold
             # check normalize
-            print(normalize_phi(flux.fixed_source_coeffs/normalization, mesh.edges, ws, N_ang, M, N_space, N_groups), 'should be 1')
+            print(normalize_phi(norm_integrand/normalization, mesh.edges, ws, N_ang, M, N_space, N_groups), 'should be 1')
             if normalization > 0:
                 flux.fixed_source_coeffs = flux.fixed_source_coeffs / normalization
                 initialize.fixed_source_coeffs = flux.fixed_source_coeffs / normalization
-                initialize.IC = initialize.IC #/ normalization
+                initialize.IC = initialize.IC / normalization
                 flux.make_fixed_phi(mesh.edges)
 
 

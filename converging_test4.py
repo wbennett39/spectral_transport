2ce567f539a72c3049bb30dd9e6433166e851958
William Bennett <wbennett@lanl.gov>
2025-07-31 08:22:34 -0600

    hpc

--- converging_test4.py
+++ converging_test4.py
@@ -66,7 +66,7 @@ import numpy as np
 
 
 
-def get_results(ts = [-94.706889, -27.126998, -1], N_spaces = [500], N_ang = 8, MM = 1):
+def get_results(ts = [-94.706889, -27.126998, -1], N_spaces = [320], N_ang = 8, MM = 1):
     from moving_mesh_transport.solver_functions.run_functions import run
     menis_times = np.array(ts)
     # N_spaces_list = [45]
